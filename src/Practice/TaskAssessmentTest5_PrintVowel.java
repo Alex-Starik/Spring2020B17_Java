ceb2ad6495bd2efe6d0b4542f2f6143efc0b0110
Alex-Starik <oleksandrstarik@gmail.com>
2020-04-27 16:58:48 -0400

    Day29

--- src/Practice/TaskAssessmentTest5_PrintVowel.java
+++ src/Practice/TaskAssessmentTest5_PrintVowel.java
@@ -1,4 +1,21 @@
 package Practice;
 
+import java.util.Scanner;
+
 public class TaskAssessmentTest5_PrintVowel {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.print("In:");
+        String word = inp.nextLine();
+        //write your code below
+        String result = "";
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) == 'e' || word.charAt(i) == 'u' || word.charAt(i) == 'i' || word.charAt(i) == 'o' ||
+                    word.charAt(i) == 'a') {
+                result += word.charAt(i);
+            }
+        }
+
+        System.out.println(result);
+    }
 }
